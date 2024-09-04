238413726451c31c5af9b429a1733a79b6a6c629
diana <diana_letter_12@mail.ru>
2024-09-04 12:38:50 +0300

    progress (midday)

--- SerializationHomework/Assets/Scripts/PositionSaver.cs
+++ SerializationHomework/Assets/Scripts/PositionSaver.cs
@@ -9,7 +9,7 @@ namespace DefaultNamespace
     [System.Serializable]
     public class PositionSaver : MonoBehaviour
 	{
-		[Serializable]        
+		[System.Serializable]        
         public struct Data
 		{
 			public Vector3 Position;
